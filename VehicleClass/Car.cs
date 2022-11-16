326fc8099291376a6ccabef9e67fd965f9eb7665
gemma <gemma@DESKTOP-2Q2G917>
2022-11-16 11:03:22 +0000

    Add project files.

---
+++ VehicleClass/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VehicleClass
+{
+    class Car
+    {
+        //Method and properties
+        public void Description(string name, string vehicleType, string colour, string price)
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(vehicleType);
+            Console.WriteLine(colour);
+            Console.WriteLine(price);
+        }
+    }
+}
---
+++ VehicleClass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VehicleClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The first car is called:");
+            Car descriptionCar1 = new();
+            descriptionCar1.Description("Fer", "Convertable", "Red", "£60,000.00");
+
+            Console.WriteLine("\nThe second car is called:");
+            Car descriptionCar2 = new();
+            descriptionCar2.Description("Jump", "Van", "Blue", "£10,000.00");
+        }
+    }
+}
